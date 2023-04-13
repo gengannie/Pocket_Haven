e0ab9c1076dc086b477ee9315397e7b1c3d716a8
gengannie <annie1112002@gmail.com>
2023-04-13 16:39:17 -0700

    Starter project

---
+++ src/src/srcApp.swift
@@ -0,0 +1,17 @@
+//
+//  srcApp.swift
+//  src
+//
+//  Created by Annie Geng on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct srcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
